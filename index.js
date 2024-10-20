dc5c664c22e83416c2e0343d9b704235188578df
gouthamsaji <gouthamsaji@gmail.com>
2024-10-20 10:45:10 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+//import json server
+const jsonserver = require('json-server')
+
+//create server
+const mediaPlayerServer = jsonserver.create()
+
+//create middleware to convert the json format
+const middleware = jsonserver.defaults()
+
+//import db.json file
+const router = jsonserver.router("db.json")
+
+//set port for server
+const PORT = 4000 || process.env.PORT
+
+//server use middleware
+mediaPlayerServer.use(middleware)
+mediaPlayerServer.use(router)
+
+//listen
+mediaPlayerServer.listen(PORT,()=>{
+    console.log(`Server runnning at ${PORT}`)
+})
